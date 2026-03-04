111b9b7b51a21d17333cfe965eb7744a8288e7fd
biancademambro <biancademambro12@hotmail.com>
2026-03-04 20:05:06 -0300

    extra

--- exercicio_dicionario.py
+++ exercicio_dicionario.py
@@ -1,17 +1,30 @@
-nome = input("Qual produto que deseja?")
-preco = input("Qual o preço? ")
-estoque = input("Quantos há em estoque? ")
-categoria = input("Qual a categoria do produto? ")
+lista_produtos =[]
 
-produtos =     {
-    "nome": nome,
-    "preco": preco,
-    "estoque": estoque,
-    "categoria": categoria
+while True: 
 
-}
+    nome = input("Qual produto que deseja?")
+    preco = float(input("Qual o preço? "))
+    estoque = input("Quantos há em estoque? ")
+    categoria = input("Qual a categoria do produto? ")
 
-print(produtos["nome"])
-print(produtos["preco"])
-print(produtos["estoque"])
-print(produtos["categoria"])
+    produtos =     {
+     "nome": nome,
+     "preco": preco,
+     "estoque": estoque,
+     "categoria": categoria
+
+    }
+
+    lista_produtos.append(produtos)
+
+    print(produtos["nome"])
+    print(produtos["preco"])
+    print(produtos["estoque"])
+    print(produtos["categoria"])
+
+    pergunta = input("Você deseja adicionar mais produtos? Digite S ou N: ").upper()
+
+    if pergunta != "S":
+        print("Encerrando o cadastro...")
+        print(lista_produtos)
+        break
